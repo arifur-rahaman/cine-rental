8c66580a3a24ad09a3019f1bf92ed9d060820ab9
arifur-rahaman <arif.rahman041@gmail.com>
2024-10-26 21:08:49 +0600

    refactor: add MovieCard component under MovieList

--- src/utilities/cinema-utilities.js
+++ src/utilities/cinema-utilities.js
@@ -1,4 +1,8 @@
 function getImgUrl(name) {
+  // this is utils function will create dynamic image path or url even if the data comes from database;
+  // first we have to create URL object
+  // it takes 2 parameter: pathname and import.meta.url from vite
+  // finally we have to return its .href value
   return new URL(`../assets/movie-covers/${name}`, import.meta.url).href;
 }
 export { getImgUrl };
